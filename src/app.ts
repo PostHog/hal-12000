f3c96d5f4ef76c7868fd5d57be02e58e4cea6818
Michael Matloka <dev@twixes.com>
2022-11-10 12:28:00 +0100

    Update test response

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,7 @@
-import { api as pdApi} from '@pagerduty/pdjs';
+import { api as pdApi } from '@pagerduty/pdjs'
 import { App } from '@slack/bolt'
 
-
-const pd = pdApi({token: process.env.PAGERDUTY_TOKEN}) // eslint-disable-line @typescript-eslint/no-unused-vars
+const pd = pdApi({ token: process.env.PAGERDUTY_TOKEN }) // eslint-disable-line @typescript-eslint/no-unused-vars
 const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
     signingSecret: process.env.SLACK_SIGNING_SECRET,
@@ -13,11 +12,13 @@ const app = new App({
 app.command('/support-hero', async ({ command, ack, respond }) => {
     await ack()
 
-    await respond(`${command.text}`)
+    await respond(command.text || `Hi Dave!`)
 })
 
-app.start(process.env.PORT || 3000).then(() => {
-    console.log('👁️ HAL is now watching')
-}).catch((error) => {
-    console.error('🚨 HAL encountered a problem:', error)
-})
+app.start(process.env.PORT || 3000)
+    .then(() => {
+        console.log('👁️ HAL is now watching')
+    })
+    .catch((error) => {
+        console.error('🚨 HAL encountered a problem:', error)
+    })
