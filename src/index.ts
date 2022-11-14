fc08bfea587880ac6abdb70839946cacdfed297f
Michael Matloka <dev@twixes.com>
2022-11-14 17:51:58 +0100

    Fix cron

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import { scheduleJob } from 'node-schedule'
 import { app, shoutAboutCurrentSupportHero, shoutAboutNextSupportHero } from './app'
 
 // Every Monday at 7:50 AM UTC (which should be soon after the CharlieHR message)
-scheduleJob('50 6 * * 1', shoutAboutCurrentSupportHero)
+scheduleJob('50 7 * * 1', shoutAboutCurrentSupportHero)
 // Every Wednesday at 7:50 AM UTC (which should be soon after the CharlieHR message, and also before sprint planning)
-scheduleJob('1 9 * * 3', shoutAboutNextSupportHero)
+scheduleJob('50 7 * * 3', shoutAboutNextSupportHero)
 
 void app.start()
