4f1673b66bdc585e4df6fc51a78fe1adfae36ca9
Michael Matloka <dev@twixes.com>
2022-12-12 10:22:56 +0100

    Update for Roots message time

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import { scheduleJob } from 'node-schedule'
 
 import { app, shoutAboutCurrentSupportHero, shoutAboutNextSupportHero } from './app'
 
-// Every Monday at 7:50 AM UTC (which should be soon after the CharlieHR message)
-scheduleJob('50 7 * * 1', shoutAboutCurrentSupportHero)
-// Every Wednesday at 7:50 AM UTC (which should be soon after the CharlieHR message, and also before sprint planning)
-scheduleJob('50 7 * * 3', shoutAboutNextSupportHero)
+// Every Monday at 7:15 AM UTC (which should be soon after the Roots message)
+scheduleJob('15 7 * * 1', shoutAboutCurrentSupportHero)
+// Every Wednesday at 7:15 AM UTC (which should be soon after the Roots message, and also before sprint planning)
+scheduleJob('15 7 * * 3', shoutAboutNextSupportHero)
 
 void app.start()
