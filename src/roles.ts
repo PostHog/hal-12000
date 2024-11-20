5769cc125676c7c9248b7fb29b8cfdc402535cb9
Michael Matloka <michael@matloka.com>
2024-11-20 12:55:20 +0100

    Allow more flexible support hero channels

--- src/roles.ts
+++ src/roles.ts
@@ -8,12 +8,17 @@ export interface Role {
 /** Secondary support people, per-team. */
 export const SUPPORT_HERO_ROLES: Role[] = process.env.SUPPORT_HERO_TEAMS_WITH_SCHEDULE_IDS
     ? process.env.SUPPORT_HERO_TEAMS_WITH_SCHEDULE_IDS.split(',').map((teamWithScheduleId) => {
-          // The format for each team is "<team-slug>:<pagerduty-schedule-id>[:<custom-role-name>]"
-          // Fake example for #team-infrastructure: "infrastructure:PIR8F1:Infra Hero"
-          const [teamSlug, scheduleId, customRoleName] = teamWithScheduleId.trim().split(':')
+          // The format for each team is "<team-channel>:<pagerduty-schedule-id>[:<custom-role-name>]".
+          // Fake example for #team-infrastructure: "team-infrastructure:PIR8F1:Infra Hero".
+          // Team channels should almost always start with `team-`, e.g. `team-foo`.
+          // This way both channels `team-foo` and `support-foo` get updates.
+          // However, if there's a support queue shared between teams, the channel can also start with `support-`,
+          // in which case only `support-foo` gets updates.
+          const [teamChannel, scheduleId, customRoleName] = teamWithScheduleId.trim().split(':')
+          const teamName = slugToTitleCase(teamChannel.replace(/^support-/, '').replace(/^team-/, ''))
           return {
-              name: customRoleName || `Support Hero for ${slugToTitleCase(teamSlug)}`,
-              channel: `team-${teamSlug}`,
+              name: customRoleName || `Support Hero for ${teamName}`,
+              channel: teamChannel,
               scheduleId,
           }
       })
