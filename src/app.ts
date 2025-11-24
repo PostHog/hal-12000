c05b592cddc89b5c2b0825b2477b80289ff13ee1
Michael Matloka <michael@matloka.com>
2025-11-24 12:28:48 +0100

    Remove Pagerduty code
    Replaced by https://github.com/PostHog/shared-actions/pull/21

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,7 @@
 import { captureException } from '@sentry/node'
 import { App } from '@slack/bolt'
-import { UsersLookupByEmailResponse } from '@slack/web-api'
 
 import { kudosGive, kudosShow } from './kudos'
-import { Role } from './roles'
-import { supportHeroSet, supportHeroShow } from './support'
 
 export const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
@@ -12,27 +9,6 @@ export const app = new App({
     appToken: process.env.SLACK_APP_TOKEN,
     socketMode: true,
 })
-
-export async function fetchSlackMentionByEmail(userToFind: { name: string; email: string } | null): Promise<string> {
-    if (!userToFind) {
-        return '⚠️ No one scheduled'
-    }
-    let userFound: UsersLookupByEmailResponse['user']
-    try {
-        const lookupResponse = await app.client.users.lookupByEmail({ email: userToFind.email })
-        userFound = lookupResponse.user
-        if (lookupResponse.error) {
-            captureException(lookupResponse.error)
-        }
-    } catch (error) {
-        captureException(error)
-    }
-    return userFound?.id ? `<@${userFound.id}>` : userToFind.name
-}
-
-export function linkifyRoleName(role: Pick<Role, 'scheduleId' | 'name'>): string {
-    return `<https://posthog.pagerduty.com/schedules/${role.scheduleId}|${role.name}>`
-}
 
 // eslint-disable-next-line @typescript-eslint/require-await
 app.error(async (error) => {
@@ -48,23 +24,5 @@ app.command('/kudos', async ({ command, ack, respond }) => {
         await kudosShow(command, respond, args.slice(1))
     } else {
         await kudosGive(command, respond, args)
-    }
-})
-
-app.command('/support-hero', async ({ command, ack, respond }) => {
-    await ack()
-
-    if (!command.channel_name.startsWith('team-') && !command.channel_name.startsWith('feature-')) {
-        await respond({
-            text: '/support-hero can only be used in channels that start with `team-` or `feature-`!',
-            response_type: 'ephemeral',
-        })
-        return
-    }
-
-    if (!command.text.trim()) {
-        await supportHeroShow(command, respond)
-    } else {
-        await supportHeroSet(command, respond)
     }
 })
--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,7 @@
-import '@sentry/tracing' // Importing @sentry/tracing patches the global hub for tracing to work.
-
-import * as Sentry from '@sentry/node'
 import { RecurrenceRule, scheduleJob } from 'node-schedule'
 
 import { app } from './app'
-import { shoutAboutCurrentOnCall, shoutAboutUpcomingOnCall } from './oncall'
-import { fetchSupportHeroRoles } from './roles'
-import { shoutAboutCurrentSupportCastMember, shoutAboutUpcomingSupportCastMembers } from './support'
 import { shoutAboutTipOfTheWeek } from './tip-of-the-week'
-
-Sentry.init({
-    dsn: process.env.SENTRY_DSN,
-    tracesSampleRate: 1.0,
-})
-
-export async function shoutAboutCurrentCast(): Promise<void> {
-    const supportHeroRoles = await fetchSupportHeroRoles()
-    const results = await Promise.allSettled([
-        ...supportHeroRoles.map(shoutAboutCurrentSupportCastMember),
-        shoutAboutCurrentOnCall(),
-    ])
-    for (const result of results) {
-        if (result.status === 'rejected') {
-            Sentry.captureException(result.reason)
-        }
-    }
-}
-
-export async function shoutAboutUpcomingCast(): Promise<void> {
-    const supportHeroRoles = await fetchSupportHeroRoles()
-    const results = await Promise.allSettled([
-        ...supportHeroRoles.map(shoutAboutUpcomingSupportCastMembers),
-        shoutAboutUpcomingOnCall(),
-    ])
-    for (const result of results) {
-        if (result.status === 'rejected') {
-            Sentry.captureException(result.reason)
-        }
-    }
-}
 
 // Every Monday at 9:00 AM London time, #general (2 hours after the Time Off message in #general)
 const tipOfTheWeekRule = new RecurrenceRule()
@@ -47,21 +10,5 @@ tipOfTheWeekRule.hour = 9
 tipOfTheWeekRule.minute = 0
 tipOfTheWeekRule.tz = 'Europe/London'
 scheduleJob(tipOfTheWeekRule, shoutAboutTipOfTheWeek)
-
-// Every Monday at 7:00 AM London time, #dev (same time as the Time Off message in #general)
-const currentCastRule = new RecurrenceRule()
-currentCastRule.dayOfWeek = 1
-currentCastRule.hour = 7
-currentCastRule.minute = 0
-currentCastRule.tz = 'Europe/London'
-scheduleJob(currentCastRule, shoutAboutCurrentCast)
-
-// Every Wednesday at 7:00 AM London time, #dev (same time as the Time Off message in #general)
-const upcomingCastRule = new RecurrenceRule()
-upcomingCastRule.dayOfWeek = 3
-upcomingCastRule.hour = 7
-upcomingCastRule.minute = 0
-upcomingCastRule.tz = 'Europe/London'
-scheduleJob(upcomingCastRule, shoutAboutUpcomingCast)
 
 void app.start()
