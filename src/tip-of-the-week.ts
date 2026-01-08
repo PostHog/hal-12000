677f6b2020d775bc5e7312220a9c33e3c89847b1
Robbie <robbie.coomber@gmail.com>
2026-01-08 14:04:53 +0000

    Fix typo and add hedgehog fact to tips (#11)
    * Fix typo and add hedgehog fact to tips
    
    Corrected a typo and added a new tip about hedgehogs.
    
    * Fix typo in tip of the week message
    
    * Update tip for all hands recording timing
    
    * Fix typo in tip of the week message

--- src/tip-of-the-week.ts
+++ src/tip-of-the-week.ts
@@ -51,6 +51,9 @@ const POSTHOG_TIPS: Tip[] = [
         "If you don't want to wait for the all hands recording to be posted in #general it gets added to the calendar event shortly after the meeting ends",
         "Now you have no excuse to miss everybodys favourite weekly TV show",
     ],
+    [
+        "Hedgehogs are not rodents! They are part of the Erinaceinae subfamily, which is a group of mammals. It is considered rude to tell a hedgehog that it is a rodent.
+    ]
 ]
 
 function getCurrentTipOfTheWeek(): Tip {
